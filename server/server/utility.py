84205997a557355342d80ad1b8e1febf65506017
havidalbar@gmail.com <havidalbar@gmail.com>
2020-09-21 21:08:32 +0700

    add evaluation param cluster 2 in threshold

--- server/server/utility.py
+++ server/server/utility.py
@@ -5,12 +5,12 @@ from main_algorithm.vector_space_model import VectorSpaceModel
 from main_algorithm.fcm import fcm_clustering
 
 def load_model_pickle():
-    pickle_filename: str = 'main_algorithm/resources/vector_space_model_surah_pendek.pickle'
+    pickle_filename: str = 'main_algorithm/resources/vector_space_model_surah_gabungan.pickle'
     result_data_file = open(pickle_filename, 'rb')
     result_model = pickle.load(result_data_file)
     result_data_file.close()
                 
-    pickle_filename_cluster = 'main_algorithm/resources/clusterSurahPendek.pickle'
+    pickle_filename_cluster = 'main_algorithm/resources/clusterSurahGabungan.pickle'
     cluster_file = open(pickle_filename_cluster, 'rb')
     list_cluster = pickle.load(cluster_file)
     cluster_file.close()
